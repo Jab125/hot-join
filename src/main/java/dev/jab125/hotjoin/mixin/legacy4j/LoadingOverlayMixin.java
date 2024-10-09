67352753de8aac9e054a2558ef9d4f0e7b2654db
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-10-09 17:26:46 +1100

    Update LoadingOverlayMixin.java

--- src/main/java/dev/jab125/hotjoin/mixin/legacy4j/LoadingOverlayMixin.java
+++ src/main/java/dev/jab125/hotjoin/mixin/legacy4j/LoadingOverlayMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.LoadingOverlay;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,16 +17,18 @@ import java.util.Optional;
 
 @Mixin(value = LoadingOverlay.class, priority = 999)
 public class LoadingOverlayMixin {
+	@Unique
+	private boolean hotjoin$cleared = false;
 	@Inject(method = "render", at = @At("HEAD"))
 	void render(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) throws IllegalAccessException {
-		if (HotJoin.hotjoinClient) {
+		if (HotJoin.hotjoinClient && !hotjoin$cleared) {
 			Optional<Field> loadIntroLocation = Arrays.stream(LoadingOverlay.class.getDeclaredFields()).filter(a -> a.getName().contains("loadIntroLocation")).findFirst();
 			Field field = loadIntroLocation.orElseThrow();
 			field.setAccessible(true);
 			field.set((LoadingOverlay) (Object) this, true);
-			System.out.println("Cleared intros");
 			LegacyResourceManager.INTROS.clear();
 			LegacyResourceManager.INTROS.add(ResourceLocation.fromNamespaceAndPath("legacy", "textures/gui/intro/background.png"));
+			hotjoin$cleared = true;
 		}
 	}
 }
