f9deea0b176212e05243b804daf9ab020380a0e1
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-10-21 21:16:16 +1100

    fix a controlify error

--- src/main/java/dev/jab125/hotjoin/compat/controlify/ControlifyEntrypoint.java
+++ src/main/java/dev/jab125/hotjoin/compat/controlify/ControlifyEntrypoint.java
@@ -3,6 +3,7 @@ package dev.jab125.hotjoin.compat.controlify;
 import dev.isxander.controlify.Controlify;
 import dev.isxander.controlify.api.ControlifyApi;
 import dev.isxander.controlify.controller.ControllerEntity;
+import dev.jab125.hotjoin.HotJoin;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -10,6 +11,8 @@ import java.util.Optional;
 public class ControlifyEntrypoint implements dev.isxander.controlify.api.entrypoint.ControlifyEntrypoint {
 	@Override
 	public void onControllersDiscovered(ControlifyApi controlify) {
+		if (!HotJoin.hotjoinClient) return;
+		if (ControlifyModCompat.payload == null) return;
 		ControlifyData data = ControlifyModCompat.payload.data();
 		Optional<ControllerEntity> first = Controlify.instance().getControllerManager().orElseThrow().getConnectedControllers().stream().filter(a -> Objects.equals(data.uid(), a.info().uid())).findFirst();
 		if (first.isPresent()) {
